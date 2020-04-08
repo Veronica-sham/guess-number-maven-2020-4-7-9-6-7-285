466069712d892815440e41bd6130044936edcf12
Veronica Sham <shamve@oocl.com>
2020-04-08 21:07:02 +0800

    [Veronica]-pass test should_Confirm_Input_From_User_Is_Right

--- src/main/java/com/oocl/GuessNumber.java
+++ src/main/java/com/oocl/GuessNumber.java
@@ -14,14 +14,41 @@ public class GuessNumber {
 
     public String generate4RandomNumber() {
         ArrayList<Integer> numberList = new ArrayList<Integer>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        List<Integer> generated ;
+        List<Integer> generatedNumber ;
         String combine = "";
         Collections.shuffle(numberList);
-        generated= numberList.subList(0,4);
+        generatedNumber= numberList.subList(0,4);
 
-        for (Integer s : generated) {
-            combine += s.toString();
+        for (Integer number : generatedNumber) {
+            combine += number.toString();
         }
         return combine;
+    }
+
+    public Boolean getInputNumber(){
+        List<String> inputNo;
+        Boolean isInputRight = false;
+        String InputRight = "You entered string ";
+        String InputWrong = "Wrong Input, Input again";
+        do {
+            System.out.println("Input 4 numbers in the format of xxxx(e.g.1234): ");
+            Scanner consoleInput = new Scanner(System.in);
+            String inputNumber = consoleInput.nextLine();
+            inputNo = Arrays.asList(inputNumber.split(""));
+            if (inputNo.size() == inputNo.stream().distinct().count() && inputNo.size() == 4) {
+                InputRight = InputRight + inputNumber;
+                System.out.println(InputRight);
+                isInputRight = true;
+            } else {
+                System.out.println(InputWrong);
+            }
+        } while(inputNo.size() != inputNo.stream().distinct().count() || inputNo.size() != 4);
+        return isInputRight;
+    }
+
+    public void compareInputValid(String input){
+        List<String> inputNo;
+        inputNo = Arrays.asList(input.split(""));
+
     }
 }
--- src/test/java/com/oocl/GuessNumberTest.java
+++ src/test/java/com/oocl/GuessNumberTest.java
@@ -3,16 +3,22 @@ package com.oocl;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
 public class GuessNumberTest {
+
 
     public static void main(String[] args){
 
         GuessNumber guessNumber = new GuessNumber();
         String generatedNumber = guessNumber.generate4RandomNumber();
         System.out.println(generatedNumber);
+        System.out.println(guessNumber.getInputNumber());
+
+
     }
 
   /*  @Test
@@ -27,15 +33,25 @@ public class GuessNumberTest {
     } */
 
     @Test
-    public void return_Non_Repeated_Random_Number(){
+    public void should_return_Non_Repeated_Random_Number(){
         GuessNumber guessNumber = new GuessNumber();
-        List<String> listss;
+        List<String> checkNumberList;
         String generatedNumber = guessNumber.generate4RandomNumber();
         Boolean nonRepeat = false;
-        listss = Arrays.asList(generatedNumber.split(""));
-        if (listss.size() == listss.stream().distinct().count()){
+        checkNumberList = Arrays.asList(generatedNumber.split(""));
+        if (checkNumberList.size() == checkNumberList.stream().distinct().count()){
             nonRepeat = true;
         }
-        System.out.println(generatedNumber);
+        Assert.assertEquals(true, nonRepeat);
     }
+
+    @Test
+    public void should_Confirm_Input_From_User_Is_Right(){
+        GuessNumber guessNumber = new GuessNumber();
+        String SimulatedInput = "1379";
+        InputStream in = new ByteArrayInputStream(SimulatedInput.getBytes());
+        System.setIn(in);
+        Assert.assertEquals(true,guessNumber.getInputNumber());
+    }
+
 }
