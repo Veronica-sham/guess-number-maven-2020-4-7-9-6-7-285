47cbbc7b6eafa6eeb142a435f24c1e8e4c37450e
Veronica-sham <56880714+Veronica-sham@users.noreply.github.com>
2020-04-10 17:12:35 +0800

    Add files via upload

---
+++ src/main/java/com/oocl/StartGame.java
@@ -0,0 +1,27 @@
+package com.oocl;
+
+public class StartGame {
+
+    public void startingTheGame(){
+        int attemptTime = 0;
+        String compareResult = "";
+
+        GuessNumber guessNumber = new GuessNumber();
+        String generatedNumber = guessNumber.generate4RandomNumber();
+        //System.out.println("generated number: " + generatedNumber);
+        do {
+            String userInput = guessNumber.getInputNumber();
+            compareResult = guessNumber.compareInputWithRandom(userInput, generatedNumber);
+            System.out.println(compareResult);
+            if (compareResult.equals("4A0B")) {
+                System.out.println("you win");
+                break;
+            }
+            if (!compareResult.equals("4A0B") && attemptTime == 5) {
+                System.out.println("you lose. The answer is : "+generatedNumber);
+            }
+            attemptTime++;
+
+        } while (attemptTime <= 5);
+    }
+}
