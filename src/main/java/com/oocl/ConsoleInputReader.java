34a4fb06db47c0e66900a85e2f9b9adf0c6a6b01
Veronica Sham <shamve@oocl.com>
2020-04-14 18:42:31 +0800

    [Veronica]- reformat code

--- src/main/java/com/oocl/ConsoleInputReader.java
+++ src/main/java/com/oocl/ConsoleInputReader.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.oocl;
 
 import java.util.Scanner;
@@ -9,16 +8,5 @@ public class ConsoleInputReader implements InputReader {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
-=======
-package com.oocl;
 
-import java.util.Scanner;
-
-public class ConsoleInputReader implements InputReader {
-    @Override
-    public String getInput() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
-    }
->>>>>>> 7387c087334dcf6f55f54d94482e8f9ebc6bed70
 }
--- src/main/java/com/oocl/InputReader.java
+++ src/main/java/com/oocl/InputReader.java
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 package com.oocl;
 
 public interface InputReader {
     String getInput();
-=======
-package com.oocl;
 
-public interface InputReader {
-    String getInput();
->>>>>>> 7387c087334dcf6f55f54d94482e8f9ebc6bed70
 }
